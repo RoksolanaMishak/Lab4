d5b7faf48c0f8676b5b6f0bbb63da4677aeb676d
Roksolana Mishak <r.mishak@gmail.com>
2019-04-10 11:40:49 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component} from 'react';
+import { AppRegistry,Alert, Text, TextInput,StyleSheet,FlatList,Button,View } from 'react-native';
 
-export default class App extends React.Component {
+
+export default class PizzaTranslator extends Component {
+  constructor(probs){
+    super(probs);
+    this.state = {text: ''};
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{padding: 40}}>
+       <FlatList
+          data={[
+            {key: 'Молоко'},
+            {key: 'Хліб'},
+            {key: 'Сир'},
+            {key: 'Масло'},
+            {key: 'Помідор'},
+            {key: 'Огірок'},
+            {key: 'Капуста'},
+            {key: 'Буряк'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+        <Button
+  onPress={() => {
+    Alert.alert('ЗАМОВЛЕННЯ ПРИЙНЯТО');
+  }}
+  title="Купити"
+/>
+        <TextInput style={{heigt: 10}}
+        placeholder="Поле для введення"
+        onChangeText={(text) => this.setState({text})} />
+        <Text style={{padding:20, fontSize: 50}}>
+        {this.state.text.split(' ').map ((word) => word && '').join(' ')}
+        </Text>
+       
+       
+  
       </View>
     );
   }
 }
+AppRegistry.registerComponent('FirstApp', ()=>PizzaTranslator);
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
   },
-});
+  item: {
+    backgroundColor: 'yellow',
+    padding: 15,
+    fontSize: 20,
+    height: 50,
+  },
+})
